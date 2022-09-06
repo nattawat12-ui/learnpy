578fcc92a6e024fcf3b794a7dc42a67e3ff05a34
Your Name <you@example.com>
2022-09-07 01:51:15 +0700

    TRYIT

--- Tryit!/nestif.py
+++ Tryit!/nestif.py
@@ -2,11 +2,11 @@ earn = int(input('Enter you earnings: '))
 if earn >= 15000:
     print('Pass')
     if earn < 70000:
-        print('Silver card')
-    if earn < 100000:
-        print('Gold card')
-    if earn > 100000:
-        print('Platinum card')
+        print('Silver Card')
+    elif earn < 100000 :
+        print('Gold Card')
+    elif earn > 100000:
+        print('Platinum Card')
             
 else:
         print('Not pass')
