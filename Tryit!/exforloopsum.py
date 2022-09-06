5e3994cf29be9a24eeb0081c39abbf5633ef1ed5
Your Name <you@example.com>
2022-09-07 01:59:32 +0700

    TRYIT

--- Tryit!/exforloopsum.py
+++ Tryit!/exforloopsum.py
@@ -1,4 +1,9 @@
-number = int(input('Enter number: '))
+#number = int(input('Enter number: '))
 #disply = sum(1+2+3+4+5+6+7+8+9+10)
-x = sum(range(1,number+1))
-print(x)
+#x = sum(range(1,number+1))
+#print(x)
+number = int(input('Enter number: '))
+s=0
+for i in range(1,number+1,1):
+    s+=i
+print('Sum:',s)
